498da0ff5f1c960b2c6bf6b59f7425cd7efae067
GrimaceOfDespair <GrimaceOfDespair@hotmail.com>
2012-11-02 21:55:58 +0100

    Add Jeremy.Console to send log messages by entering text in a console

---
+++ Jeremy.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using log4net;
+
+namespace Jeremy.Console
+{
+  class Program
+  {
+    public static ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("Type anything followed by carriage return to send it to log4net");
+
+      string line;
+      while (string.IsNullOrEmpty(line = System.Console.ReadLine()) == false)
+      {
+        Log.Debug(line);
+      }
+    }
+  }
+}
